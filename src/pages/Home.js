9df27087964aa7b8ccc79415c9ebda45521f6cd9
Rajkumaryogi <rajkumar6777y@gmail.com>
2025-04-06 10:05:38 +0530

    link Change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
   const fetchProducts = async () => {
     // final base_url = process.env.
     try {
-        const url = 'http://localhost:4002/product';
+        const url = 'auth-login-backend.vercel.app/product';
         const headers = {
           method: 'GET',
           headers: {
